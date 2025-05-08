b3ffbee752737021daf49806877c866d3154a167
Ryan <89214160+rkimbers@users.noreply.github.com>
2025-05-07 18:50:12 -0700

    addded support for order side

--- src/main/scala/books/OrderBook.scala
+++ src/main/scala/books/OrderBook.scala
@@ -2,97 +2,51 @@ package books
 
 scala.collection.mutable
 
-class OrderBook {
-  private val priceLevels = mutable.TreeMap[Long, mutable.Queue[OrderNode]]()(Ordering[Long].reverse)  //buy side
+class OrderBook(isBuySide: Boolean) {
+
+  private val ordering =
+    if (isBuySide) Ordering[Long].reverse
+    else Ordering[Long]
+
+  private val priceLevels = mutable.TreeMap[Long, mutable.Queue[OrderNode]]()(ordering)
   private val orderMap = mutable.HashMap[Long, OrderNode]()
   private var bestPrice: Option[Long] = None
 
   def addOrder(order: OrderNode): Unit = {
     val price = order.price
-
     val queue = priceLevels.getOrElseUpdate(price, mutable.Queue[OrderNode]())
-
+    queue.enqueue(order)
     orderMap(order.guid) = order
 
-    if(bestPrice.isEmpty() || price > bestPrice.get){
-      bestPrice = Some(price)
-    }
+    bestPrice = Some(priceLevels.head._1)
   }
 
   def cancelOrder(orderId: Long): Boolean = {
     orderMap.get(orderId) match {
       case Some(order) =>
         val price = order.price
-        val queue = priceLevels(price)package books
+        val queue = priceLevels(price)
 
-    scala.collection.mutable
+        queue.dequeueFirst(_.guid == orderId)
 
-    class OrderBook {
-    private val priceLevels = mutable.TreeMap[Long, mutable.Queue[OrderNode]]()(Ordering[Long].reverse)  //buy side
-    private val orderMap = mutable.HashMap[Long, OrderNode]()
-    private var bestPrice: Option[Long] = None
-
-    def addOrder(order: OrderNode): Unit = {
-    val price = order.price
-
-    val queue = priceLevels.getOrElseUpdate(price, mutable.Queue[OrderNode]())
-
-    orderMap(order.guid) = order
-
-    if(bestPrice.isEmpty() || price > bestPrice.get){
-    bestPrice = Some(price)
-    }
-    }
-
-    def cancelOrder(orderId: Long): Boolean = {
-    orderMap.get(orderId) match {
-    case Some(order) =>
-    val price = order.price
-    val queue = priceLevels(price)
-
-    queue.dequeueFirst(_.guid == orderId)
-
-    if(queue.isEmpty){
-    priceLevels -= price
-    }
-
-    orderMap -= orderId
-
-    if(bestPrice.contains(price) && queue.isEmpty){
-    bestPrice = priceLevels.headOption.map(_._1)
-    }
-
-    true
-
-    case Node =>
-    false
-    }
-    }
-
-    def getBestPrice: Option[Long] = bestPrice
-    def getBestPrice: Option[Long] = priceLevels.headOption.map(_._1)
-    }
-
-
-    queue.dequeueFirst(_.guid == orderId)
-
-        if(queue.isEmpty){
+        if (queue.isEmpty) {
           priceLevels -= price
         }
 
         orderMap -= orderId
 
-        if(bestPrice.contains(price) && queue.isEmpty){
-          bestPrice = priceLevels.headOption.map(_._1)
-        }
+        bestPrice = priceLevels.headOption.map(_._1)
 
         true
 
-      case Node =>
+      case None =>
         false
     }
   }
 
   def getBestPrice: Option[Long] = bestPrice
-  def getBestPrice: Option[Long] = priceLevels.headOption.map(_._1)
+
+  def peekBestOrder: Option[OrderNode] = {
+    priceLevels.headOption.flatMap(_._2.headOption)
+  }
 }
