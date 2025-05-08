23bb31c0c0c91625c1508c2c311c133fb58781e9
Ryan <89214160+rkimbers@users.noreply.github.com>
2025-05-07 18:17:30 -0700

    completed order node

--- src/main/scala/books/OrderNode.scala
+++ src/main/scala/books/OrderNode.scala
@@ -1,10 +1,12 @@
+package books
+
 case class OrderNode(
     priceNode: Node,
     guidNode: Node,
     var amount: Long,
     stockId: Long,
     kind: MsgKind,
-    var nextFree: Option[OderNode] = None
+    var nextFree: Option[OrderNode] = None
 ) {
 
     def copyFrom(from: Message): Unit = {
@@ -34,17 +36,17 @@ case class OrderNode(
 
     def traderId: Int = highInt32(guid)
 
-    def tradeId: Int lowInt32(guid)
+    def tradeId: Int = lowInt32(guid)
     
-    def amount(): Long = amount
+    def amount: Long = amount
 
     def reduceAmount(s: Long): Unit = {
         amount -= s
     }
 
-    def stockId(): Long = stockId
+    def stockId: Long = stockId
 
-    def kind(): MsgKind = kind()
+    def kind: MsgKind = kind()
 
     def remove(): Unit = {
     priceNode.pop()
