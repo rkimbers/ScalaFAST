d51d1721f90afe42560788e5e2cbc960e3c976b3
Ryan <89214160+rkimbers@users.noreply.github.com>
2025-05-07 18:27:04 -0700

    BUY side completed

--- src/main/scala/books/OrderBook.scala
+++ src/main/scala/books/OrderBook.scala
@@ -1,22 +1,98 @@
+package books
+
+scala.collection.mutable
+
 class OrderBook {
-  
+  private val priceLevels = mutable.TreeMap[Long, mutable.Queue[OrderNode]]()(Ordering[Long].reverse)  //buy side
+  private val orderMap = mutable.HashMap[Long, OrderNode]()
+  private var bestPrice: Option[Long] = None
+
+  def addOrder(order: OrderNode): Unit = {
+    val price = order.price
+
+    val queue = priceLevels.getOrElseUpdate(price, mutable.Queue[OrderNode]())
+
+    orderMap(order.guid) = order
+
+    if(bestPrice.isEmpty() || price > bestPrice.get){
+      bestPrice = Some(price)
+    }
+  }
+
+  def cancelOrder(orderId: Long): Boolean = {
+    orderMap.get(orderId) match {
+      case Some(order) =>
+        val price = order.price
+        val queue = priceLevels(price)package books
+
+    scala.collection.mutable
+
+    class OrderBook {
+    private val priceLevels = mutable.TreeMap[Long, mutable.Queue[OrderNode]]()(Ordering[Long].reverse)  //buy side
+    private val orderMap = mutable.HashMap[Long, OrderNode]()
+    private var bestPrice: Option[Long] = None
+
+    def addOrder(order: OrderNode): Unit = {
+    val price = order.price
+
+    val queue = priceLevels.getOrElseUpdate(price, mutable.Queue[OrderNode]())
+
+    orderMap(order.guid) = order
+
+    if(bestPrice.isEmpty() || price > bestPrice.get){
+    bestPrice = Some(price)
+    }
+    }
+
+    def cancelOrder(orderId: Long): Boolean = {
+    orderMap.get(orderId) match {
+    case Some(order) =>
+    val price = order.price
+    val queue = priceLevels(price)
+
+    queue.dequeueFirst(_.guid == orderId)
+
+    if(queue.isEmpty){
+    priceLevels -= price
+    }
+
+    orderMap -= orderId
+
+    if(bestPrice.contains(price) && queue.isEmpty){
+    bestPrice = priceLevels.headOption.map(_._1)
+    }
+
+    true
+
+    case Node =>
+    false
+    }
+    }
+
+    def getBestPrice: Option[Long] = bestPrice
+    def getBestPrice: Option[Long] = priceLevels.headOption.map(_._1)
+    }
+
+
+    queue.dequeueFirst(_.guid == orderId)
+
+        if(queue.isEmpty){
+          priceLevels -= price
+        }
+
+        orderMap -= orderId
+
+        if(bestPrice.contains(price) && queue.isEmpty){
+          bestPrice = priceLevels.headOption.map(_._1)
+        }
+
+        true
+
+      case Node =>
+        false
+    }
+  }
+
+  def getBestPrice: Option[Long] = bestPrice
+  def getBestPrice: Option[Long] = priceLevels.headOption.map(_._1)
 }
-
-
-/* 
-Todo: 
-
--Price-time priority
--Support fast insertions
--Retrieve best bid
--Support cancellations
-
- */
-
-/* 
-Data Structures:
-RBT to store sorted price levels and range queries
-Heap to find best price (pqueue)
-HashMap + LL for price lookup
-
- */
